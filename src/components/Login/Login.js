86ef0c8cbad468b59bd69981c39800be4f022de6
Quyen Diep <2014datquyen@gmail.com>
2026-03-24 00:16:52 +0700

    9.3.2 ✔ CSS giao diện form login ✔ Tạo giao diện trên Mobile/Desktop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,25 @@
 import './Login.scss';
 const Login = () => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className='container'>
-                <div className='row'>
-                    <div className='content-left col-7'>
-                        <div className='branch'>
-                            Hoi Dan IT
+                <div className='row px-3 px-sm-0'>
+                    <div className='content-left col-sm-7 d-none d-sm-block'>
+                        <div className='brand'>
+                            facebook
                         </div>
                         <div className='detail'>
-                            Learning everything
+                            Facebook helps you connect and share with the people in your life.
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+                        <div className='brand d-sm-none text-center'>
+                            facebook
+                        </div>
                         <input type="text" className="form-control" placeholder="Email address or phone number" />
                         <input type="password" className="form-control" placeholder="Password" />
                         <button className="btn btn-primary">Login</button>
-                        <span className="text-center">Forgot your password?</span>
+                        <span className="text-center"><a className="forgot-password" href="#">Forgot your password?</a></span>
                         <hr />
                         <div className="text-center">
                             <button className="btn btn-success">Create new account</button>
