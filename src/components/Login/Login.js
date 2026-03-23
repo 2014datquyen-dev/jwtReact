4af143c235ae8a45fc9245555a82f457d4b0cf51
Quyen Diep <2014datquyen@gmail.com>
2026-03-23 22:10:56 +0700

    9.3.1 ✔ Tạo Form Login ✔ Sử dụng Bootstrap 5 để dàn layout

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,29 @@
+import './Login.scss';
 const Login = () => {
     return (
-        <div className="Login-container">
-            Login
-            <button>
-                test boostrap
-            </button>
+        <div className="login-container mt-3">
+            <div className='container'>
+                <div className='row'>
+                    <div className='content-left col-7'>
+                        <div className='branch'>
+                            Hoi Dan IT
+                        </div>
+                        <div className='detail'>
+                            Learning everything
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input type="text" className="form-control" placeholder="Email address or phone number" />
+                        <input type="password" className="form-control" placeholder="Password" />
+                        <button className="btn btn-primary">Login</button>
+                        <span className="text-center">Forgot your password?</span>
+                        <hr />
+                        <div className="text-center">
+                            <button className="btn btn-success">Create new account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
