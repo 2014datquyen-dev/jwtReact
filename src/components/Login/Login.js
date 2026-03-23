c43a957ed238d8df64b7f484322637731153f3a4
Quyen Diep <2014datquyen@gmail.com>
2026-03-23 15:26:52 +0700

    video9.2 ✔ Tạo thanh Navigation cho ứng dụng React ✔ Tích hợp React-router-dom để điều hướng trang ✔ Tích hợp Bootstrap 5

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+        <div className="Login-container">
+            Login
+            <button>
+                test boostrap
+            </button>
+        </div>
+    )
+}
+
+export default Login;
